e037eb8bcd3f14361bb0c2ae24e4a77a86805f7f
LeadSeason <pauli.tiittanen113@gmail.com>
2023-07-01 19:44:09 +0300

    Made some changes

--- planetside/planetside.py
+++ planetside/planetside.py
@@ -1,9 +1,90 @@
-import discord
-import auraxium
 import asyncio
+from datetime import datetime
 from time import time
-from auraxium import ps2
-from redbot.core import Config, commands, app_commands
+
+import aiohttp
+import discord
+from redbot.core import Config, app_commands, commands
+
+from .static.weapons import WEAPONS
+
+
+class Player():
+    def __init__(self):
+        self.characterID = dict()
+        self.character = dict()
+        self.hist = dict()
+        self.stats = dict()
+        self.honuData = dict()
+        self.honuMeta = dict()
+        self.weaponStats = dict()
+
+
+# @TODO Use indexed dict
+FACTIONS = [
+    {
+        # Yeah I know is a dumb solution but hey it works
+        "tag": None,
+        "name": None,
+        "color": 0x000000,
+        "emoji": None
+    },
+    {
+        "tag": "VS",
+        "name": "Vanu Sovereignty",
+        "color": 0xc061cb,
+        "emoji": "<:vs:441405448113881098>"
+    },
+    {
+        "tag": "NC",
+        "name": "New Conglomerate",
+        "color": 0x62a0ea,
+        "emoji": "<:nc:441405432091901972>"
+    },
+    {
+        "tag": "TR",
+        "name": "Terran Republic",
+        "color": 0xed333b,
+        "emoji": "<:tr:1104394643145232395>"
+    },
+    {
+        "tag": "NSO",
+        "name": "Nanite Systems Operatives",
+        "color": 0x777777,
+        "emoji": "<:nso:938862172522573904>"
+    }
+]
+
+CLASSES = {
+    1: "Infiltrator",
+    3: "Light Assault",
+    4: "Combat Medic",
+    5: "Engineer",
+    6: "Heavy Assault",
+    7: "MAX"
+}
+
+SERVERS = [
+    "connery",
+    "miller",
+    "cobalt",
+    "emerald",
+    "jaeger",
+    "soltech",
+    "genudine",
+    "ceres"
+]
+
+SERVER_IDS = {
+    1: "Connery",
+    10: "Miller",
+    13: "Cobalt",
+    17: "Emerald",
+    19: "Jaeger",
+    40: "SolTech",
+    1000: "Genudine",
+    2000: "Ceres"
+}
 
 
 class Planetside(commands.Cog):
@@ -15,110 +96,170 @@ class Planetside(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=2162678924, force_registration=True)
 
+        self.honuUrl = "https://wt.honu.pw"
+
         self.config.register_global(
             serviceId=False
         )
 
-    @commands.command()
-    @commands.is_owner()
-    async def setserviceid(self, ctx, newServiceId):
-        await self.config.serviceId.set(newServiceId)
-        await ctx.reply("Service ID has ben set", delete_after=10, ephemeral=True)
+    async def division(self, a, b):
+        return a / b if b else 0
 
-    @commands.command()
-    @commands.is_owner()
-    async def getserviceid(self, ctx):
-        serviceId = await self.config.serviceId()
-        await ctx.reply(f"Service ID is {serviceId}", delete_after=10, ephemeral=True)
+    async def getByName(self, username: str, server=None):
+        """
+            Gets characters stats.
+        """
+        char = Player()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=f"{self.honuUrl}/api/characters/search/{username}?censusTimeout=true") as response:
+                if response.status != 200:
+                    raise Exception(f"Got some wacky error from honu: {response.status}")
+
+                responseJson = await response.json()
+
+                if len(responseJson) == 0:
+                    return None
+
+                if server is not None:
+                    for index, x in enumerate(responseJson):
+                        if x["worldID"] != server:
+                            responseJson.pop(index)
+
+                char.characterID = responseJson[0]["id"]
+                charName = responseJson[0]["name"]
+                # If exact name found use instead of the last online
+                for x in responseJson:
+                    if x["name"].lower() == username:
+                        char.characterID = x["id"]
+                        charName = x["name"]
+
+            # @TODO Make happen all at the same time not one by one
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}") as response:
+                if response.status == 200:
+                    char.character = await response.json()
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}/stats") as response:
+                if response.status == 200:
+                    char.stats = await response.json()
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}/history_stats") as response:
+                if response.status == 200:
+                    char.hist = await response.json()
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}/honu-data") as response:
+                if response.status == 200:
+                    char.honuData = await response.json()
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}/metadata") as response:
+                if response.status == 200:
+                    char.honuMeta = await response.json()
+            async with session.get(url=f"{self.honuUrl}/api/character/{char.characterID}/weapon_stats") as response:
+                if response.status == 200:
+                    char.weaponStats = await response.json()
+        return char
 
     @app_commands.command()
-    @app_commands.describe(username=" of the Player")
-    async def playerstats(self, interaction: discord.Interaction, username: str):
+    @app_commands.describe(username="Look up a player's stats.")
+    async def stats(self, interaction: discord.Interaction, username: str):
         """
             Get stats for a planetside player
         """
-        # Using defer because sensus reponses are sometimes slow
+        # Using defer because census responses are sometimes slow
         await interaction.response.defer(thinking=True)
 
         startTime = time()
 
-        serviceId = await self.config.serviceId()
-        if serviceId is False:
-            # Maybe add some error here? maybe from redbot
+        char = await self.getByName(username.lower())
+
+        if char is None:
+            await interaction.edit_original_response(
+                embed=discord.Embed(title=f"Failed to find player '{username}'", color=0xed333b)
+            )
+            await asyncio.sleep(5)
+            await interaction.delete_original_response()
             return
 
-        async with auraxium.Client(service_id=f"s:{serviceId}") as client:
-            try:
-                char = await client.get_by_name(ps2.Character, username.lower())
-            except auraxium.errors.ServiceUnavailableError:
-                await interaction.edit_original_response(
-                    embed=discord.Embed(title="Failed to connect to census api", color=0xed333b)
-                )
-                await asyncio.sleep(5)
-                await interaction.delete_original_response()
-                return
-
-            if char is None:
-                await interaction.edit_original_response(
-                    embed=discord.Embed(title=f"Failed to find player '{username}'", color=0xed333b)
-                )
-                await asyncio.sleep(5)
-                await interaction.delete_original_response()
-                return
-
-            onlineIcon = "🔴"
-            if await char.is_online():
+        onlineIcon = "🔴"
+        if "online" in char.honuData:
+            if char.honuData["online"]:
                 onlineIcon = "🟢"
 
-            if char.faction_id == 1:
-                # faction_name = "VS"
-                faction_color = 0xc061cb
-                faction_emoji = "<:vs:441405448113881098>"
-            elif char.faction_id == 2:
-                # faction_name = "NC"
-                faction_color = 0x62a0ea
-                faction_emoji = "<:nc:441405432091901972>"
-            elif char.faction_id == 3:
-                # faction_name = "TR"
-                faction_color = 0xed333b
-                faction_emoji = "<:tr:1104394643145232395>"
-            else:
-                # faction_name = "NSO"
-                faction_color = 0x777777
-                faction_emoji = "<:nso:938862172522573904>"
+        factionEmoji = FACTIONS[char.character['factionID']]['emoji']
+        factionName = FACTIONS[char.character['factionID']]['name']
+        factionColor = FACTIONS[char.character['factionID']]['color']
+        serverName = SERVER_IDS[char.character['worldID']]
 
-            outfit = await char.outfit()
-            charTitle = await char.title()
+        # If in outfit display outfit in [91AR]
+        outfitTag = ""
+        if char.character["outfitTag"] is not None:
+            outfitTag = f"[{char.character['outfitTag']}] "
 
-            # If in outfit display outfit in [OFIT]
-            outfitTag = ""
-            if outfit is not None:
-                outfitTag = f"[{outfit.tag}] "
+        prestige = ""
+        if char.character["prestige"] > 0:
+            prestige = f"~{char.character['prestige']}"
 
-            prestige = ""
-            if char.prestige_level > 0:
-                prestige = f"~{char.prestige_level}"
+        # @TODO Character title is missing not provided by honu 😥
+        embed = discord.Embed(
+            title=f"{onlineIcon} {factionEmoji} {outfitTag}{char.character['name']}",
+            description=f"""Of {serverName}'s {factionName}
+                            Battle rank {char.character['battleRank']}{prestige}""",
+            color=factionColor
+        )
 
-            if charTitle is None:
-                charTitle = ""
+        if char.character["outfitTag"] is not None:
+            embed.add_field(name='Outfit', value=f"[{char.character['outfitTag']}] {char.character['outfitName']}", inline=False)
 
-            """
-                🟢 :nc: Character
-                Title BR 120
-            """
-            embed = discord.Embed(
-                title=f"{onlineIcon} {faction_emoji} {outfitTag}{char.name}",
-                description=f"{charTitle} BR {char.battle_rank.value}{prestige}",
-                color=faction_color
-            )
+        # Bit complex but what ever it gets the job done
+        playTimePerClass = [0] * 10
+        mostPlayedClass = 0
+        mostPlayedTime = 0
+        for stat in char.stats:
+            if stat["statName"] == "play_time":
+                classID = stat["profileID"]
+                classPlaytime = stat["valueForever"]
 
-            if outfit is not None:
-                embed.add_field(name="Outfit", value=f"{outfitTag} {outfit.name}")
+                playTimePerClass[classID] = classPlaytime
+                if mostPlayedTime <= classPlaytime:
+                    mostPlayedClass = classID
+                    mostPlayedTime = classPlaytime
 
-            embed.add_field(name="Time played", value=f"{char.times.minutes_played // 60} Hours, {char.times.minutes_played % 60} Minutes")
-            embed.add_field(name="Player creation", value=f"<t:{char.times.creation}:D>")
-            embed.add_field(name="Last logon", value=f"<t:{char.times.last_save}:R>")
+        totalPlayTime = sum(playTimePerClass)
+        totalPlatTimeMinutes = sum(playTimePerClass) / 60
+        totalKills = 0
+        totalDeaths = 0
+        totalScore = 0
 
-            embed.set_footer(text=f"Process time: {round(time() - startTime, 2)} seconds.", icon_url="https://cdn.tims.host/2/ucsQnspf70JR7FQ.png")
+        for x in char.hist:
+            if x["type"] == "deaths":
+                totalDeaths = x["allTime"]
+            if x["type"] == "kills":
+                totalKills = x["allTime"]
+            if x["type"] == "score":
+                totalScore = x["allTime"]
 
-            await interaction.edit_original_response(embed=embed)
+        mostKillsWeaponKills = 0
+        mostKillsWeaponID = 0
+        for weapon in char.weaponStats:
+            if mostKillsWeaponKills <= weapon["stat"]["kills"]:
+                mostKillsWeaponKills = weapon["stat"]["kills"]
+                mostKillsWeaponID = int(weapon["itemID"])
+
+        embed.add_field(name="K-D ", value=f'{"{:,}".format(totalKills)} - {"{:,}".format(totalDeaths)} = {"{:,}".format(totalKills - totalDeaths)}')
+        embed.add_field(name="KDR", value=f"{round(await self.division(totalKills, totalDeaths), 2)}")
+        embed.add_field(name="KPM", value=f"{round(totalKills / totalPlatTimeMinutes, 2)}")
+
+        embed.add_field(name="Score (SPM)", value=f'{"{:,}".format(totalScore)} ({round(totalScore / totalPlatTimeMinutes, 1)})')
+
+        if mostKillsWeaponID in WEAPONS:
+            weaponName = WEAPONS[mostKillsWeaponID]["name"]
+            embed.add_field(name="Top weapon (Kills)", value=f'{weaponName} ({"{:,}".format(mostKillsWeaponKills)})')
+
+        embed.add_field(name="Time played", value=f"{int(totalPlayTime / 60 // 60)} Hours, {int(totalPlayTime // 60 % 60)} Minutes")
+
+        if "online" in char.honuData:
+            if not char.honuData["online"]:
+                embed.add_field(name='Last logon', value=f"<t:{char.honuData['latestEventTimestamp'] // 1000}:R>")
+        embed.add_field(name="Player creation", value=f"<t:{datetime.strptime(char.character['dateCreated'], '%Y-%m-%d %H:%M:%SZ').strftime('%s')}:D>")
+
+        if mostPlayedClass != 0:
+            embed.add_field(name="Most played class", value=f"**{CLASSES[mostPlayedClass]}** {int(mostPlayedTime / 60 // 60)} Hours, {mostPlayedTime // 60 % 60} Minutes")
+
+        embed.set_footer(text=f"Process time: {round(time() - startTime, 2)} seconds.", icon_url="https://cdn.tims.host/2/ucsQnspf70JR7FQ.png")
+
+        await interaction.edit_original_response(embed=embed)
